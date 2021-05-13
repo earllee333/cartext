525a16c07c52cc82e4f7355c6f4e878eeed4f6b2
earllee333 <jerrykiet2012@gmail.com>
2021-05-13 16:17:14 +0800

    loading picture

--- component/CarsMenu.js
+++ component/CarsMenu.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 const CarsMenu = ({data}) => {
-    if(!data) return <div>Loading</div>
+    if(!data) return <div>Loading Picture</div>
     const{titles,slug,number,stock,profilePicture,tag,mark} = data.fields
     
     return ( 
@@ -9,7 +9,7 @@ const CarsMenu = ({data}) => {
                 <Image src={'https:'+profilePicture.fields.file.url} 
                 width={450} height={300}></Image>
             </div>
-            <h2>{titles}</h2>
+            
         </div>
      );
 }
